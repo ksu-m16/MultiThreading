1423f49c9cd265c9799a0defab2cf9ddae4a8c95
xu <xu@00fcee48-bca6-446a-b9d5-5d3b22c4d7dc>
2012-08-21 14:53:55 +0000

    git-svn-id: svn://127.0.0.1/var/cvs/xu/MultiThreading/trunk@55 00fcee48-bca6-446a-b9d5-5d3b22c4d7dc

---
+++ src/test/BTest.java
@@ -0,0 +1,56 @@
+package test;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BTest {
+	int counter = 0;
+
+    public static void main(String[] args) {
+    	BTest btest = new BTest();
+    	synchronized(btest){
+        btest.test();
+    	}
+    }
+    
+    class CountThread extends Thread{
+    	public void run() {
+            for (int i = 0; i < 100*1000*1000; ++i) {                
+                counter++;                
+            }
+        }
+    }
+    
+    public void test() {
+        try {
+            Thread th1 = new CountThread();
+            Thread th2 = new CountThread();
+            th1.start();
+            th2.start();
+            th1.join();
+            th2.join();
+            System.out.println(counter);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(BTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+       
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
